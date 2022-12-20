9b309b7b7ad80fade3cc28d52025f319ed0eafba
Mezan2002 <mezanurr009@gmail.com>
2022-12-21 04:20:16 +0600

    Users List added

---
+++ src/Components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import Spinner from "react-bootstrap/Spinner";
+
+function Loading() {
+  return (
+    <div className="loading">
+      <Spinner animation="border" />
+    </div>
+  );
+}
+
+export default Loading;
--- src/Components/UsersList/UsersList.js
+++ src/Components/UsersList/UsersList.js
@@ -1,16 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Loading from "../Loading/Loading";
 
 function UsersList() {
+  const { data: usersList, isLoading } = useQuery({
+    queryKey: ["usersList"],
+    queryFn: async () => {
+      const res = await fetch("usersList.json");
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
   return (
     <Card className="text-center">
       <p className="cardHeader">USERS LIST</p>
       <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        {usersList.map((user) => (
+          <div
+            key={user.id}
+            className=" bg-dark text-white py-3 rounded-3 mb-2"
+          >
+            <div className="d-flex">
+              <img src={user.avatar} alt="" className="ms-3" />
+              <h5 className="ms-3">{user.firstName + " " + user.lastName}</h5>
+            </div>
+          </div>
+        ))}
       </Card.Body>
     </Card>
   );
