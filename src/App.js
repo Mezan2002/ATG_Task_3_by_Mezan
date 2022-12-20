7d3deeed384deac375f627078b1791a0aa2ad3f7
Mezan2002 <mezanurr009@gmail.com>
2022-12-21 03:36:25 +0600

    Component cards added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
+import UserDetails from "./Components/UserDetails/UserDetails";
+import UsersList from "./Components/UsersList/UsersList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <UsersList></UsersList>
+          </div>
+          <div className="col-md-6">
+            <UserDetails></UserDetails>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
